7d825527479eeff912c5202683c53c0384704633
hxrish <108225503+hxrish@users.noreply.github.com>
2023-05-01 22:56:54 +0530

    Add player selection

--- script.js
+++ script.js
@@ -11,52 +11,55 @@ let gridcellsToArr = (function (){
     }
 })(gridcells)
 
-function createPlayer(name, mark){
-    return {
+function player(name, mark, playing){
+   return{
         name,
         mark,
-        playing: function decide(){
-            if(mark == 'x'){
-                return false;
+        playing
+   }
+};
+
+let player1 = player('harish', 'o', true);
+let player2 = player('nomnom', 'x', false);
+
+
+(function whoIsplaying () {
+    let playingPlayer;
+    gridcellsToArr.arr.forEach(Element => {
+        Element.addEventListener('click', () => {
+            if (player1.playing == true){
+                Element.innerText = 'o';
+                winning();
+                playingPlayer = player1;
+                player1.playing = false;
+                player2.playing = true;
+                // console.log(playingPlayer);
             }
-            else if(mark == 'o'){
-                return true;
-            };
-        }
+            else if(player1.playing == false && player2.playing == true){
+                winning();
+                playingPlayer = player2;
+                player2.playing = false;
+                player1.playing = true;
+                // console.log(playingPlayer);
+            }
+        })
+    })
+    // console.log(playingPlayer);
+})(player1, player2);
+
+function winning(){
+    if(gridcellsToArr.arr[0].innerText === gridcellsToArr.arr[4].innerText && gridcellsToArr.arr[4].innerText === gridcellsToArr.arr[8].innerText   && gridcellsToArr.arr[8].innerText === gridcellsToArr.arr[11].innerText && gridcellsToArr.arr[8].innerText != '' && gridcellsToArr.arr[0].innerText != ''  && gridcellsToArr.arr[4].innerText != '' ){
+        console.log('win!');
+    }
+    else if(gridcellsToArr.arr[0].innerText === gridcellsToArr.arr[1].innerText && gridcellsToArr.arr[2].innerText != '' && gridcellsToArr.arr[0].innerText != ''  && gridcellsToArr.arr[1].innerText != '' ){
+        console.log('win!');
     }
 }
 
-let player1 = createPlayer('harish', 'o');
-let player2 = createPlayer('nomnom', 'x');
-
-let whoIsPlaying = (function (){
-    let playing;
-    if(player1.playing = true){
-        playing = player1;
-    }
-    else if(player1.playing = false){
-        playing = player2;
-    }
-    return playing;
-})(player1, player2);
 
 
-gridcellsToArr.arr.forEach(element => {
-    element.addEventListener('click', () => {
-        if (player1.playing = false){
-            console.log('nhji');
-            // player1.playing = true;
-            // player2.playing = false;
-            console.log(whoIsPlaying);
-        }
-        else if (player1.playing = true){
-            console.log('hi');
-            player1.playing = false;
-            player2.playing = true;
-            console.log(whoIsPlaying);
-        }
-    })
-});
+
+
 
 
 
