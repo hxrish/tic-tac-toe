71c951608829d60efa57a0287c42d3df50f467d5
hxrish <108225503+hxrish@users.noreply.github.com>
2023-05-07 12:34:24 +0530

    Add into JS folder

--- game.js
+++
@@ -1,8 +0,0 @@
-import { playerInfo } from "./script";
-
-let backToHomepage = document.getElementById('home_img');
-
-backToHomepage.addEventListener('click', () => {
-    window.location.replace('./index.html');
-})
-
